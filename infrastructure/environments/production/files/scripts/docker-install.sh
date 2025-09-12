42df296f70cc6a53624b2bf80ec80ebec72ffe77
basher83 <129473091+basher83@users.noreply.github.com>
2025-09-12 04:10:42 +0000

    [MegaLinter] Apply linters fixes

--- infrastructure/environments/production/files/scripts/docker-install.sh
+++ infrastructure/environments/production/files/scripts/docker-install.sh
@@ -14,8 +14,8 @@ chmod a+r /etc/apt/keyrings/docker.asc
 # Add the repository to Apt sources
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
-  tee /etc/apt/sources.list.d/docker.list > /dev/null
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" |
+  tee /etc/apt/sources.list.d/docker.list >/dev/null
 
 # Update package index
 apt-get update
